25b14a4c9534aee0fd8e5b0c977b4feb7b52e684
DNAngeluS <luciomf@gmail.com>
2020-01-28 16:25:40 -0300

    Fixed static files

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import next from 'next';
 import nextI18NextMiddleware from 'next-i18next/middleware';
 import express from 'express';
+import path from 'path';
 
 import nextI18next from './i18n';
 import { IncomingMessage, ServerResponse } from 'http';
@@ -16,7 +17,7 @@ const handle = app.getRequestHandler();
 	const server = express();
 
 	server.use(nextI18NextMiddleware(nextI18next));
-
+	server.use(express.static(path.join(__dirname, 'public')));
 	server.all('*', (req: IncomingMessage, res: ServerResponse): Promise<void> => handle(req, res));
 
 	await server.listen(port);
